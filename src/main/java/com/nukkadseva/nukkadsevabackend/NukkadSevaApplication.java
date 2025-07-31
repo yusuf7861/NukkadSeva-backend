d05453acf665e1c5035d3b1136a2824f054d6a06
Yusuf Jamal <72370431+yusuf7861@users.noreply.github.com>
2025-08-01 00:17:41 +0530

    first commit

---
+++ src/main/java/com/nukkadseva/nukkadsevabackend/NukkadSevaApplication.java
@@ -0,0 +1,13 @@
+package com.nukkadseva.nukkadsevabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukkadSevaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NukkadSevaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nukkadseva/nukkadsevabackend/NukkadSevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nukkadseva.nukkadsevabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukkadSevaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
