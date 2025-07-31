b257b2511fd375759d5e57463958d2279c8f39b8
Yusuf Jamal <72370431+yusuf7861@users.noreply.github.com>
2025-08-01 00:23:05 +0530

    liquibase added for proper versioning of db

---
+++ src/main/java/com/nukkadseva/nukkadsevabackend/entity/Address.java
@@ -0,0 +1,28 @@
+package com.nukkadseva.nukkadsevabackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id", nullable = false)
+    private Long id;
+
+    @Column(name = "full_address", nullable = false, length = 1000)
+    private String fullAddress;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "pin_code", nullable = false)
+    private String pincode;
+}
